bcc20a9ea2f707b5529f6484b054c3fe260c1caf
Tom Shea <thshea@microsoft.com>
2023-09-30 10:59:48 -0700

    store processes in process list

--- src/os/processes.ts
+++ src/os/processes.ts
@@ -4,12 +4,14 @@ import { Process } from "~/application/process";
 export const processes = signal<Process<unknown>[]>([]);
 
 export function createProcess<State>({ initialState, name, onOpen }: ProcessDescription<State>): Process<State> {
-    return {
+    const process = {
         state: signal(initialState),
         name,
         windows: {},
         onOpen,
     };
+    processes.value = [...processes.value, process];
+    return process;
 }
 
 export function getOrCreateProcess<State>(desc: ProcessDescription<State>): Process<State> {
